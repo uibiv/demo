bf641c0d19c95887925e8b1995dab34bdd7f0d87
vibiu <vibiu@qq.com>
2018-07-30 13:05:02 +0800

    reset

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 set -ex
 
 # see https://github.com/pypa/setuptools/issues/1257
-if [[ $TRAVIS_PYTHON_VERSION == 'nightly' ]]
-    then travis_retry pip install -U setuptools
-fi
+# if [[ $TRAVIS_PYTHON_VERSION == 'nightly' ]]
+#     then travis_retry pip install -U setuptools
+# fi
 pip install -e .[dev]
 pre-commit install
